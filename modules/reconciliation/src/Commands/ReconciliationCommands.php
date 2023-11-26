bf463973020b3f7dd950f25c710ec967dba53b2d
Maurizio Cavalletti <maurizio@akabit.it>
2023-11-26 17:02:27 +0100

    WIP: drush module for post migrations

---
+++ modules/reconciliation/src/Commands/ReconciliationCommands.php
@@ -0,0 +1,351 @@
+<?php
+declare(strict_types=1);
+
+namespace Drupal\reconciliation\Commands;
+
+use Drush\Commands\DrushCommands;
+use Drupal\node\Entity\Node;
+
+/**
+ * A Drush commandfile.
+ *
+ * In addition to this file, you need a drush.services.yml
+ * in root of your module, and a composer.json file that provides the name
+ * of the services file to use.
+ *
+ * See these files for an example of injecting Drupal services:
+ *   - http://cgit.drupalcode.org/devel/tree/src/Commands/DevelCommands.php
+ *   - http://cgit.drupalcode.org/devel/tree/drush.services.yml
+ */
+class ReconciliationCommands extends DrushCommands {
+
+  /**
+   * Drush command che sostituisce i link dei file.
+   *
+   * @param string $content_type
+   *   Argument with message to be displayed. Only for test.
+   * @command reconciliation:missedlinks
+   * @aliases rcl-missedlinks rcl-mslinks
+   * @option bundle
+   *   Node bundle.
+   *
+   * @usage reconciliation:missedlinks page
+   */
+  public function missedlinks($content_type = 'none') {
+    // entity query
+    $query = \Drupal::entityQuery('node');
+    if ("none" != $content_type) {
+      $query->condition('type', $content_type, '=');
+      $nids = $query->execute();
+      $database = \Drupal::database();
+      foreach ($nids as $nid) {
+        // $nid = 25467;
+        $db_query = $database->select('migrate_map_silfi_d7_node_page');
+        $db_query->addField('migrate_map_silfi_d7_node_page', 'sourceid1');
+        $db_query->condition('destid1', $nid);
+        $result = $db_query->execute();
+        $source_id = $result->fetchField(0);
+        // $source_id = 9826;
+        if ($source_id) {
+          $links = $this->getOldDbValues((int)$source_id);
+          if ($links) {
+            $this->insertLinks((int)$nid, $links);
+          }
+        }
+
+      }
+      $this->output()->writeln('Finito!');
+    }
+    else {
+      $this->output()->writeln('Errore! Tipo di contenuto obbligatorio.');
+    }
+  }
+
+  /**
+   * Prende i valori dal db D7.
+   *
+   * @param int $source_id
+   * @return object
+   */
+  private function getOldDbValues(int $source_id) {
+    // Switch to external database
+    \Drupal\Core\Database\Database::setActiveConnection('migrate');
+    // Get a connection going
+    $db = \Drupal\Core\Database\Database::getConnection();
+    $db_query = $db->select('field_data_field_link_esterni', 'l');
+    $db_query->fields('l', ['entity_id', 'delta', 'field_link_esterni_url', 'field_link_esterni_title']);
+    $db_query->condition('entity_id', $source_id);
+    $result = $db_query->execute();
+    foreach ($result as $record) {
+      $links[$record->delta]['uri'] = $record->field_link_esterni_url;
+      $links[$record->delta]['title'] = $record->field_link_esterni_title;
+      $links[$record->delta]['options'] = [
+        'attributes' => [
+          'target' => '_blank',
+        ],
+      ];
+    }
+    return $links;
+  }
+
+  /**
+   * Salva i link nel nodo.
+   *
+   * @param int $nid
+   * @param array $links
+   * @return void
+   */
+  private function insertLinks(int $nid, array $links) {
+    $node = Node::load($nid);
+    if ($node instanceof Node) {
+      try {
+        $node->set('field_link_utili', $links);
+        $node->save();
+      }
+      catch (\Exception $e) {
+        watchdog_exception('InsertLink', $e);
+      }
+    }
+  }
+
+  /**
+   * Replaces links from old nodes.
+   *
+   * @param       $content_type
+   *   Specify a content type for which the node links should be
+   *   replaced.
+   * @param array $options
+   *   An associative array of options whose values come from cli, aliases,
+   *   config, etc.
+   *
+   * @option  option-name
+   * @usage   reconciliation:replacelink article
+   *
+   * @command reconciliation:replacelink
+   * @aliases rclrep
+   */
+  public function commandName(
+    $content_type = "none",
+    $options = ['option-name' => 'default']
+  ) {
+    // entity query
+    $query = \Drupal::entityQuery('node');
+    if ("none" != $content_type) {
+      $query->condition('type', $content_type, '=');
+      $nids = $query->execute();
+      $database = \Drupal::database();
+      foreach ($nids as $nid) {
+        $db_query = $database->select('node__body');
+        $db_query->condition('entity_id', $nid);
+        $db_query->condition('body_value', "%node/%", "LIKE");
+        $db_query->addField('node__body', 'body_value');
+        $db_bodies = $db_query->execute();
+        foreach ($db_bodies as $dbBody) {
+          $body = $dbBody->body_value;
+          $upd_body = $this->replaceLinks($body, $nid, $content_type);
+          $db_upd = $database->update('node__body')
+            ->fields(['body_value' => $body])
+            ->condition('entity_id', $nid)
+            ->execute();
+        }
+      }
+      $this->output()->writeln('Finito!');
+    }
+    else {
+      $this->output()->writeln('Errore! Tipo di contenuto obbligatorio.');
+    }
+  }
+
+  /**
+   * Cerca occorrenze di node/1234.
+   */
+  private function replaceLinks(&$body, $nid, $content_type) {
+    // preg_match_all('/<a href="(\/*)node\/(\d+)"/', $value, $matches, PREG_SET_ORDER);
+    $pattern = '/node\/[0-9]+/';
+    $res = [];
+    $is_match = preg_match_all($pattern, $body, $res);
+    if ($is_match > 0) {
+      $sourceNids = $this->sourceNids($res, 'node');
+      $this->replaceNids($body, $sourceNids, $content_type, 'node');
+    }
+
+  }
+
+  /**
+   * Sostituisce vecchi nid con i nuovi.
+   */
+  private function replaceNids(&$body, array $sourceNids, string $content_type, string $command = 'node') {
+    $destNids = $this->getDestNids($sourceNids, $content_type, $command);
+    foreach ($sourceNids as $key => $sourceNid) {
+      if (isset($destNids[$sourceNid])) {
+        $body = str_replace($sourceNid, $destNids[$sourceNid], $body, $count);
+        $this->output()->writeln('Modificate n. occorrenze: ' . $count);
+      }
+    }
+
+  }
+
+  /**
+   * Cerca i nuovi nids dei nodi migrati.
+   */
+  private function getDestNids(array $sourceNids, string $content_type, string $command = 'node') {
+    $database = \Drupal::database();
+    switch ($command) {
+      case 'node':
+        $db_query = $database->select('migrate_map_silfi_d7_node_' . $content_type);
+        $db_query->condition('sourceid1', $sourceNids, 'IN');
+        $db_query->fields('migrate_map_silfi_d7_node_'. $content_type, ['sourceid1', 'destid1']);
+        break;
+
+      default:
+        # code...
+        break;
+    }
+
+    return $db_query->execute()->fetchAllKeyed();
+  }
+
+  /**
+   * Ritorna un array di nids.
+   */
+  private function sourceNids(array $res, string $command = 'node') {
+    $sourceNids = [];
+    foreach ($res[0] as $key => $value) {
+      $pieces = \explode('/', $value);
+      switch ($command) {
+        case 'node':
+          $sourceNids[$pieces[1]] = $pieces[1];
+          break;
+
+        case 'file':
+          $sourceNids[$pieces[2]] = $pieces[2];
+          break;
+      }
+    }
+    return $sourceNids;
+  }
+
+  /**
+   * Drush command che sostituisce i link dei file.
+   *
+   * @param string $content_type
+   *   Argument with message to be displayed. Only for test.
+   * @command reconciliation:filelink
+   * @aliases rcl-filelink rcl-flink
+   *
+   * @option site Site URL.
+   *
+   * @usage reconciliation:filelink
+   */
+  public function filelink($content_type = 'none', $options = ['site' => null]) {
+    // entity query
+    $query = \Drupal::entityQuery('node');
+    if ("none" != $content_type && $options['site']) {
+      $query->condition('type', $content_type, '=');
+      $nids = $query->execute();
+      $database = \Drupal::database();
+      foreach ($nids as $nid) {
+        // $nid = 17128;
+        $db_query = $database->select('node__body');
+        $db_query->condition('entity_id', $nid);
+        $db_query->condition('field_descrizione_completa', "%/file/%/download%", "LIKE");
+        $db_query->addField('node__body', 'body_value');
+        $db_bodies = $db_query->execute();
+        foreach ($db_bodies as $dbBody) {
+          $body = $dbBody->body_value;
+          $this->replaceFiles($body, $options);
+          $database->update('node__body')
+            ->fields(['body_value' => $body])
+            ->condition('entity_id', $nid)
+            ->execute();
+        }
+      }
+      $this->output()->writeln('Finito con i link!');
+    }
+    else {
+      $this->output()->writeln('Errore! Tipo di contenuto e site obbligatorio.');
+    }
+  }
+
+  /**
+   * Cerca occorrenze di /file/12345/download.
+   */
+  private function replaceFiles(&$body, $options) {
+    // preg_match_all('/<a href="(\/*)node\/(\d+)"/', $value, $matches, PREG_SET_ORDER);
+    $pattern = '/\/file\/[0-9]+\/download/';
+    $res = [];
+    $is_match = preg_match_all($pattern, $body, $res);
+    if ($is_match > 0) {
+      $fids = $this->sourceNids($res, 'file');
+      $i = 0;
+      foreach ($fids as $fid => $value) {
+        $destination = $this->getOldFileName($fid, $options);
+        $body = str_replace($res[0][$i], '/' . $destination, $body);
+        $i++;
+      }
+    }
+
+  }
+
+  /**
+   * Undocumented function
+   *
+   * @param [type] $fid
+   * @return void
+   */
+  private function getOldFileName($fid, $options) {
+    // Switch to external database
+    \Drupal\Core\Database\Database::setActiveConnection('migrate');
+    // Get a connection going
+    $db = \Drupal\Core\Database\Database::getConnection();
+    $db_query = $db->select('file_managed', 'f');
+    $db_query->fields('f', ['uri', 'filename']);
+    $db_query->condition('fid', $fid);
+    $result = $db_query->execute();
+    foreach ($result as $record) {
+      $url = str_replace(
+        'public://',
+        'https://www.comune.pontassieve.fi.it/sites/'. $options['site'] .'/files/',
+        $record->uri
+      );
+
+      $destination = 'sites/'. $options['site'] .'/files/' . $record->filename;
+      try {
+        /** @var GuzzleHttp\Psr\Response $response */
+        $response = \Drupal::httpClient()->get($url, ['sink' => $destination]);
+        if ($response->getStatusCode() == 200) {
+          return $destination;
+        }
+        else return;
+      } catch (\Throwable $th) {
+        $this->output()->writeln($th->__toString());
+      }
+
+    }
+    return;
+  }
+
+  /**
+   * Drush command that displays the given text.
+   *
+   * @param string $text
+   *   Argument with message to be displayed. Only for test.
+   * @command reconciliation:message
+   * @aliases d9-message d9-msg
+   * @option uppercase
+   *   Uppercase the message.
+   * @option reverse
+   *   Reverse the message.
+   * @usage reconciliation:message --uppercase --reverse drupal8
+   */
+  // public function message($text = 'Hello world!', $options = ['uppercase' => FALSE, 'reverse' => FALSE]) {
+  //   if ($options['uppercase']) {
+  //     $text = strtoupper($text);
+  //   }
+  //   if ($options['reverse']) {
+  //     $text = strrev($text);
+  //   }
+  //   $this->output()->writeln($text);
+  // }
+
+}
